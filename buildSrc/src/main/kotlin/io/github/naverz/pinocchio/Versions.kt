05a57020084eb23a085a29ab4931535d7720d2c0
LakeLab <todaypoc@gmail.com>
2023-11-12 14:30:02 +0900

    Support Compose version 1.3.X

--- buildSrc/src/main/kotlin/io/github/naverz/pinocchio/Versions.kt
+++ buildSrc/src/main/kotlin/io/github/naverz/pinocchio/Versions.kt
@@ -1,7 +1,7 @@
 package io.github.naverz.pinocchio
 
 object Versions {
-    const val ANDROID_COMPILE_SDK_NO = 32
-    const val ANDROID_TARGET_SDK_NO = 32
-    const val COMPOSE_VERSION = "1.2.0"
+    const val ANDROID_COMPILE_SDK_NO = 33
+    const val ANDROID_TARGET_SDK_NO = 33
+    const val COMPOSE_VERSION = "1.3.1"
 }
